f2b0807aa9628643cac8f69be7f8104ff5d16e86
Keegan Kelly <keegankelly1@gmail.com>
2021-04-07 15:51:32 -0500

    Fixed function to get  historical data.

--- index.js
+++ index.js
@@ -9,26 +9,25 @@ const alpaca = new Alpaca({
 const init = async () => {
 
 // GET ACCOUNT
-alpaca.getAccount()
-    .then((account) => {
-        if (account.trading_blocked) {
-            console.log('Account is currently restricted from trading.')
-        }
+// alpaca.getAccount()
+//     .then((account) => {
+//         if (account.trading_blocked) {
+//             console.log('Account is currently restricted from trading.')
+//         }
 
-        console.log(`Current buying power: $${account.buying_power}`)
-        console.log(`Current portfolio value: $${account.portfolio_value}`)
-        console.log(`Today's portfolio balance change: $${account.equity - account.last_equity}`)
-    })
+//         console.log(`Current buying power: $${account.buying_power}`)
+//         console.log(`Current portfolio value: $${account.portfolio_value}`)
+//         console.log(`Today's portfolio balance change: $${account.equity - account.last_equity}`)
+//     })
 
-}
 
 //GET ASSET
-alpaca.getAsset('PLTR')
-    .then((asset) => {
-        if (asset.tradable) {
-            console.log(`PLTR is tradable`)
-        }
-    })
+// alpaca.getAsset('PLTR')
+//     .then((asset) => {
+//         if (asset.tradable) {
+//             console.log(`PLTR is tradable`)
+//         }
+//     })
 
 // GET ASSETS 
 // alpaca.getAssets({
@@ -42,10 +41,10 @@ alpaca.getAsset('PLTR')
 // })
 
 // GET CLOCK
-alpaca.getClock()
-    .then(clock => {
-        clock.is_open ? console.log(`Market is open. It will close at ${clock.next_close}`) : console.log(`Market is closed. It will open at ${clock.next_open}`);
-    })
+// alpaca.getClock()
+//     .then(clock => {
+//         clock.is_open ? console.log(`Market is open. It will close at ${clock.next_close}`) : console.log(`Market is closed. It will open at ${clock.next_open}`);
+//     })
 
 // GET CALENDAR 
 // alpaca.getCalendar({
@@ -56,17 +55,14 @@ alpaca.getClock()
 //     })
 
 //GET BARS
-alpaca.getBars('day', 'PLTR', {
-    limit: 5
-}).then(barset => {
-    const pltr_barset = barset['PLTR']
-    console.log(pltr_barset[0].openPrice)
-    console.log(pltr_barset.slice(-1)[0].closePrice)
-    console.log((pltr_barset.slice(-1)[0].closePrice - pltr_barset[0].openPrice) /  pltr_barset[0].openPrice * 100)
-})
-
-const Alpaca = require('@alpacahq/alpaca-trade-api')
-const alpaca = new Alpaca()
+// alpaca.getBars('day', 'PLTR', {
+//     limit: 5
+// }).then(barset => {
+//     const pltr_barset = barset['PLTR']
+//     console.log(pltr_barset[0].openPrice)
+//     console.log(pltr_barset.slice(-1)[0].closePrice)
+//     console.log((pltr_barset.slice(-1)[0].closePrice - pltr_barset[0].openPrice) /  pltr_barset[0].openPrice * 100)
+// })
 
 // Get daily price data for AAPL over the last 5 trading days.
 const barset = alpaca.getBars(
@@ -79,32 +75,33 @@ const barset = alpaca.getBars(
     const aapl_bars = barset['AAPL']
 
     // See how much AAPL moved in that timeframe.
-    const week_open = aapl_bars[0].o
-    const week_close = aapl_bars.slice(-1)[0].c
+    const week_open = aapl_bars[0].openPrice
+    const week_close = aapl_bars.slice(-1)[0].closePrice
     const percent_change = (week_close - week_open) / week_open * 100
 
     console.log(`AAPL moved ${percent_change}% over the last 5 days`)
 })
 
-// Submit a market order to buy 1 share of Apple at market price
-alpaca.createOrder({
-    symbol: 'AAPL',
-    qty: 1,
-    side: 'buy',
-    type: 'market',
-    time_in_force: 'day'
-})
+// // Submit a market order to buy 1 share of Apple at market price
+// alpaca.createOrder({
+//     symbol: 'AAPL',
+//     qty: 1,
+//     side: 'buy',
+//     type: 'market',
+//     time_in_force: 'day'
+// })
 
-// Submit a limit order to attempt to sell 1 share of AMD at a
-// particular price ($20.50) when the market opens
-alpaca.createOrder({
-    symbol: 'AMD',
-    qty: 1,
-    side: 'sell',
-    type: 'limit',
-    time_in_force: 'opg',
-    limit_price: 20.50
-})
+// // Submit a limit order to attempt to sell 1 share of AMD at a
+// // particular price ($20.50) when the market opens
+// alpaca.createOrder({
+//     symbol: 'AMD',
+//     qty: 1,
+//     side: 'sell',
+//     type: 'limit',
+//     time_in_force: 'opg',
+//     limit_price: 20.50
+// })
 
+}
 
 init();
